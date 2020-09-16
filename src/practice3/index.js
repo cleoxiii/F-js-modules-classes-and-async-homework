5aea7ef6f157a5ca04fd39590d194c046755bce6
Yun Chenxi <cleoxiii@163.com>
2020-09-16 20:54:41 +0800

    [yunchenxi] feat: finish all practice.

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,12 @@
-export const getPoetry
+export const getPoetry = () => {
+  const url = "https://v1.jinrishici.com/all.json";
+  return fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const result = [];
+      result.push(data.origin);
+      result.push(data.author);
+      result.push(data.content);
+      return result;
+    });
+};
